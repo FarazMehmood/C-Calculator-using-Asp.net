ccb22d5e6442af70054748521e9cf635c647fa81
Malik Faraz Mehmood <88116474+FarazMehmoood@users.noreply.github.com>
2022-06-17 13:07:39 -0700

    Add files via upload

---
+++ 19011598-099.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="19011598-099.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link  href="Stylesheet.css" rel="stylesheet" type="text/css" />
+
+    <style type="text/css">
+        .auto-style1 {
+            width: 425px;
+        }
+        .auto-style2 {
+            border-style: none;
+            border-color: inherit;
+            border-width: medium;
+            padding: 5px;
+            border-radius: 20px;
+            font-size: 20px;
+        }
+    </style>
+
+</head>
+
+<body style="height: 334px" >
+    <form id="form1" runat="server" class="auto-style1"   >
+        <asp:Label ID="Label2" runat="server" Font-Size="Larger" Text="Simple Calculator" CssClass="auto-style2"></asp:Label>
+        <br />
+        &nbsp;
+        <asp:Label ID="Label1" runat="server" Text="0" BorderStyle="Ridge" style="margin-left: 0px" Width="342px" CssClass="input" BorderColor="#999966" Font-Bold="True" Font-Size="Larger" Height="36px"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;<asp:Label ID="btntest1" runat="server" Visible="False"></asp:Label>
+        <asp:Label ID="btntest2" runat="server" Visible="False"></asp:Label>
+        &nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+
+&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblerror" runat="server" BorderColor="Black" style="margin-left: 0px" Width="358px"></asp:Label>
+        <br />
+        <br />
+        &nbsp;
+        <asp:Button ID="btnred" runat="server" CssClass="auto-style2" OnClick="btnred_Click1" Text="red" Font-Size="Small" Height="32px" Width="72px" BorderStyle="Dotted" />
+&nbsp;
+        <asp:Button ID="btngreen" runat="server" CssClass="auto-style2" OnClick="btngreen_Click" Text="green" Font-Size="Small" Height="29px" Width="78px" BorderStyle="Dotted" />
+&nbsp;<asp:Button ID="btnblue" runat="server" CssClass="auto-style2" OnClick="btnblue_Click" Text="blue" Font-Size="Small" Height="28px" Width="81px" BorderStyle="Dotted" />
+
+        &nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="btnrevert" runat="server" CssClass="auto-style2" OnClick="btnrevert_Click" Text="White Theme" Font-Size="Small" Height="33px" Width="102px" BorderStyle="Dotted" />
+
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        <br />
+        &nbsp;&nbsp;
+        <br />
+        <br />
+        <asp:Button ID="btn7" runat="server" OnClick="btn7_Click" Text="7" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btn8" runat="server" OnClick="btn8_Click" Text="8" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btn9" runat="server" OnClick="btn9_Click" Text="9" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btnplus" runat="server" OnClick="btnplus_Click" Text="+" CssClass="Button" BackColor="#669999"/>
+&nbsp;<asp:Button ID="btndone" runat="server" OnClick="btndone_Click" Text="Calculate" Width="179px" CssClass="auto-style2" BackColor="#3399FF" Font-Size="Medium" />
+        <br />
+        <br />
+        <asp:Button ID="btn4" runat="server" OnClick="btn4_Click" Text="4" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btn5" runat="server" OnClick="btn5_Click" Text="5" CssClass="Button" BackColor="#999966"/>
+&nbsp;<asp:Button ID="btn6" runat="server" OnClick="btn6_Click" Text="6" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btnminus" runat="server" OnClick="btnminus_Click" Text="-" CssClass="Button" BackColor="#669999" />
+        <asp:Button ID="btnbackspace" runat="server" OnClick="btnbackspace_Click" style="margin-left: 8px" Text="Backspace " Width="172px" CssClass="auto-style2" BackColor="#3399FF" Font-Size="Medium" />
+        <br />
+        <br />
+        <asp:Button ID="btn3" runat="server" OnClick="btn3_Click" Text="3" CssClass="Button" BackColor="#999966" />
+&nbsp;<asp:Button ID="btn2" runat="server" OnClick="btn2_Click" Text="2" CssClass="Button" BackColor="#999966" BorderColor="#CC9900" />
+        &nbsp;<asp:Button ID="btn1" runat="server" OnClick="btn1_Click" Text="1" CssClass="Button" BackColor="#999966"/>
+&nbsp;<asp:Button ID="btnmultiply" runat="server" OnClick="btnmultiply_Click" Text="*" CssClass="Button" BackColor="#669999" />
+        &nbsp;<asp:Button ID="btnup" runat="server" OnClick="btnup_Click" Text="Checkup" CssClass="auto-style2" BackColor="#3399FF" Font-Size="Small" Width="77px" />
+&nbsp;<asp:Button ID="btndown" runat="server" OnClick="btndown_Click" Text="Check Down" CssClass="auto-style2" Width="99px" BackColor="#3399FF" Font-Size="Small"/>
+        <br />
+        <br />
+        <asp:Button ID="btndecimal" runat="server" OnClick="btndecimal_Click" Text="." CssClass="Button" BackColor="#669999"/>
+&nbsp;<asp:Button ID="btn0" runat="server" OnClick="btn0_Click" Text="0" CssClass="Button" BackColor="#999966" BorderColor="#FF9900" />
+&nbsp;<asp:Button ID="btnmod" runat="server" Text="%" CssClass="Button" OnClick="btnmod_Click" BackColor="#669999" />
+&nbsp;<asp:Button ID="btndivide" runat="server" OnClick="btndivide_Click" Text="/" CssClass="Button" BackColor="#669999"/>
+        &nbsp;
+        <asp:Button ID="btnclear" runat="server" OnClick="btnclear_Click" Text="clear" Width="172px" CssClass="auto-style2" BackColor="#3399FF" Font-Size="Medium" Height="31px"/>
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ 19011598-099.aspx.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    
+    string operand1="";
+    string operand2="";
+    decimal result;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblerror.Text = "";
+
+    }
+
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+        if(Label1.Text=="0")
+        {
+            Label1.Text = "7";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "7";
+        }
+
+        form1.Attributes["class"] = "buttonn";
+
+    }
+
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "8";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "8";
+        }
+    }
+
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "9";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "9";
+        }
+    }
+
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "4";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "4";
+        }
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "5";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "5";
+        }
+    }
+
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "6";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "6";
+        }
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "3";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "3";
+        }
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "2";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "2";
+        }
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "1";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "1";
+        }
+    }
+
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "0";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "0";
+        }
+    }
+
+
+    protected void btndecimal_Click(object sender, EventArgs e)
+    {
+        if(Label1.Text.Contains("."))
+        {
+            lblerror.Text = "Cant Include DECIMAL 2nd time in Same Value";
+            return;
+        }
+
+        else if (Label1.Text.IndexOf(".") < 1 )
+        {
+            Label1.Text = Label1.Text + ".";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + ".";
+        }
+    }
+
+    protected void btnmod_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text == "0")
+        {
+            Label1.Text = "%";
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + "%";
+        }
+    }
+
+    protected void btnclear_Click(object sender, EventArgs e)
+    {
+        Label1.Text = btn0.Text;
+        btn0.Enabled = true;
+        btn1.Enabled = true;
+        btn2.Enabled = true;
+        btn3.Enabled = true;
+        btn4.Enabled = true;
+        btn6.Enabled = true;
+        btn5.Enabled = true;
+        btn7.Enabled = true;
+        btn8.Enabled = true;
+        btn9.Enabled = true;
+        btnbackspace.Enabled = true;
+        btndivide.Enabled = true;
+        btnminus.Enabled = true;
+        btnmultiply.Enabled = true;
+        btnplus.Enabled = true;
+        btnmod.Enabled = true;
+        btndecimal.Enabled = true;
+
+    }
+
+    protected void btnplus_Click(object sender, EventArgs e)
+    {
+
+        if (Label1.Text.EndsWith("+") || Label1.Text.EndsWith("-") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("/") || Label1.Text.EndsWith("%"))
+        {
+            lblerror.Text = "cannot enter two operands side by side";
+            return;
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + btnplus.Text;
+        }
+
+    }
+
+    protected void btnminus_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text.EndsWith("+") || Label1.Text.EndsWith("-") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("/") || Label1.Text.EndsWith("%"))
+        {
+            lblerror.Text = "cannot enter two operands side by side";
+            return;
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + btnminus.Text;
+        }
+    }
+
+    protected void btnmultiply_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text.EndsWith("+") || Label1.Text.EndsWith("-") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("/") || Label1.Text.EndsWith("%"))
+        {
+            lblerror.Text = "cannot enter two operands side by side";
+            return;
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + btnmultiply.Text;
+        }
+    }
+
+    protected void btndivide_Click(object sender, EventArgs e)
+    {
+        if (Label1.Text.EndsWith("+") || Label1.Text.EndsWith("-") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("/") || Label1.Text.EndsWith("%"))
+        {
+            lblerror.Text = "cannot enter two operands side by side";
+            return;
+        }
+
+        else
+        {
+            Label1.Text = Label1.Text + btndivide.Text;
+        }
+    }
+
+    protected void btndone_Click(object sender, EventArgs e)
+    {
+        btntest1.Text = Label1.Text;
+        btn0.Enabled = false;
+        btn1.Enabled= false;
+        btn2.Enabled= false;
+        btn3.Enabled = false;
+        btn4.Enabled= false;
+        btn6.Enabled= false;
+        btn5.Enabled = false;
+        btn7.Enabled= false;
+        btn8.Enabled= false;
+        btn9.Enabled= false;
+        btnbackspace.Enabled = false;
+        btndivide.Enabled= false;
+        btnminus.Enabled= false;
+        btnmultiply.Enabled= false;
+        btnplus.Enabled= false;
+        btnmod.Enabled= false;
+        btndecimal.Enabled= false;
+
+
+
+        if (Label1.Text.EndsWith("+") || Label1.Text.EndsWith("-") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("*") || Label1.Text.EndsWith("%"))
+        {
+            lblerror.Text = "Invalid Operation!! , Unsufficient Operands";
+            return;
+        }
+
+        if (Label1.Text.Contains("+"))
+        {
+            
+            char[] seperator = { '+' };
+            string[] str = Label1.Text.Split(seperator);
+
+            foreach (string x in str)
+            {
+                if (operand1.Length==0)
+                {
+                    operand1 = x;
+                }
+
+                operand2 = x;
+
+            }
+
+            decimal v = Convert.ToDecimal(operand1) + Convert.ToDecimal(operand2);
+            result = v;
+            Label1.Text = Convert.ToString(result);
+          
+        }
+
+        if (Label1.Text.Contains("-"))
+        {
+
+            char[] seperator = { '-' };
+            string[] str = Label1.Text.Split(seperator);
+
+            foreach (string x in str)
+            {
+                if (operand1.Length == 0)
+                {
+                    operand1 = x;
+                    
+                }
+
+                operand2 = x;
+
+            }
+
+            result = Convert.ToDecimal(operand1) - Convert.ToDecimal(operand2);
+            Label1.Text = Convert.ToString(result);
+           
+        }
+
+        if (Label1.Text.Contains("*"))
+        {
+
+            char[] seperator = { '*' };
+            string[] str = Label1.Text.Split(seperator);
+
+            foreach (string x in str)
+            {
+                if (operand1.Length == 0)
+                {
+                    operand1 = x;
+                }
+
+                operand2 = x;
+
+            }
+
+            result = Convert.ToDecimal(operand1) * Convert.ToDecimal(operand2);
+            Label1.Text = Convert.ToString(result);
+            
+        }
+
+        if (Label1.Text.Contains("/"))
+        {
+
+            char[] seperator = { '/' };
+            string[] str = Label1.Text.Split(seperator);
+
+            foreach (string x in str)
+            {
+                if (operand1.Length == 0)
+                {
+                    operand1 = x;
+                }
+
+                operand2 = x;
+
+            }
+
+            result = Convert.ToDecimal(operand1) / Convert.ToDecimal(operand2);
+            Label1.Text = Convert.ToString(result);
+
+        }
+
+        if (Label1.Text.Contains("%"))
+        {
+
+            char[] seperator = { '%' };
+            string[] str = Label1.Text.Split(seperator);
+
+            foreach (string x in str)
+            {
+                if (operand1.Length == 0)
+                {
+                    operand1 = x;
+                }
+
+                operand2 = x;
+
+            }
+
+            result = Convert.ToDecimal(operand1) % Convert.ToDecimal(operand2);
+            Label1.Text = Convert.ToString(result);
+
+        }
+        btntest2.Text = Label1.Text;
+    }
+
+    protected void btnup_Click(object sender, EventArgs e)
+    {
+        Label1.Text = btntest1.Text;
+    }
+
+    protected void btndown_Click(object sender, EventArgs e)
+    {
+        Label1.Text = btntest2.Text;
+    }
+
+    protected void btnbackspace_Click(object sender, EventArgs e)
+    {
+     
+       if (Label1.Text.Length > 0 && Label1.Text != "0")
+        {
+            Label1.Text = Label1.Text.Remove(Label1.Text.Length - 1);
+
+            if(Label1.Text.Length==0)
+            {
+                Label1.Text = "0";
+            }
+            return;
+        }
+        
+    }
+
+    protected void btnred_Click1(object sender, EventArgs e)
+    {
+        form1.Style.Add("background-color", "red");
+    }
+
+    protected void btngreen_Click(object sender, EventArgs e)
+    {
+        form1.Style.Add("background-color", "green");
+    }
+
+    protected void btnblue_Click(object sender, EventArgs e)
+    {
+        form1.Style.Add("background-color", "blue");
+    }
+
+    protected void btnrevert_Click(object sender, EventArgs e)
+    {
+        form1.Style.Add("background-color", "lightgrey");
+    }
+}
